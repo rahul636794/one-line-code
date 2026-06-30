f63d8bc27cdc649bf6663768bd6ca10f150e3286
Rahul Singh <rahulrathore6367944740@gmail.com>
2026-06-30 12:03:21 +0530

    user name section add file

---
+++ js/javascript.js
@@ -0,0 +1,35 @@
+// let marks = 32 ;
+
+// if(marks >= 90 && marks <= 100){
+//     console.log('grade A')
+// }
+// if(marks >= 70 && marks <= 89){
+//     console.log('grade B')
+// }
+// if(marks >= 60 && marks <= 69){
+//     console.log('grade C')
+// }
+// if(marks >= 40 && marks <= 59){
+//     console.log('grade D')
+// }
+
+// if(marks >= 0 && marks <= 39){
+//     console.log('grade F')
+// }
+
+
+// let gameNumaber = '15' ;
+
+// let gusses = prompt('guss the cruct number');
+
+
+// for(gusses ; gusses != gameNumaber; gusses){
+//     gusses = prompt('you wrong')
+// }
+// console.log('you won');
+
+let gusses = prompt('enter the full name') ;
+let icon = '@' ;
+let fulllenght = Math.floor(Math.random() * 999 ) + 1 ;
+
+console.log(icon+gusses+fulllenght) ;

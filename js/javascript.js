03171c4bd9d7cefab8c5614dcf3d70fd808ba26f
Rahul Singh <rahulrathore6367944740@gmail.com>
2026-06-30 15:01:57 +0530

    code add codebase

--- js/javascript.js
+++ js/javascript.js
@@ -26,10 +26,71 @@
 // for(gusses ; gusses != gameNumaber; gusses){
 //     gusses = prompt('you wrong')
 // }
-// console.log('you won');
+// console.log('you won') ;
 
-let gusses = prompt('enter the full name') ;
-let icon = '@' ;
-let fulllenght = Math.floor(Math.random() * 999 ) + 1 ;
+// let gusses = prompt('enter the full name') ;
+// let icon = '@' ;
+// let fulllenght = Math.floor(Math.random() * 999 ) + 1 ;
 
-console.log(icon+gusses+fulllenght) ;
+// console.log(icon+gusses+fulllenght) ;
+
+// let str = 'rahul singh' ;
+// let arr = ['rahul singh', 'rahul rajput', 'mohit mishra', 'anju kanwer'] ;
+
+// for(let i of str){
+//     // console.log(i) ;
+// } ;
+
+// let marks = [85, 97, 44, 37, 76, 60] ;
+// let str = 0 ;
+
+// for(let i = 0 ; i <= marks.length ; i++){
+//         str = str + marks[i] ;
+// }
+// let str1 = str / marks.length ;
+
+// console.log(str1) ;
+
+
+ let marks = [85, 97, 44, 37, 76, 60] ;
+
+
+//  for(let a of marks){
+//   let  offer = a / 10 ;
+//   let orvalue = a - offer ;
+//    console.log(orvalue) ;
+// } 
+
+// let str = 'rahul' ;
+
+// let profile = {
+//     name : 'rahul',
+//     class : 12 ,
+//     bio : 'this is same bio'
+// } ;
+// let sym = Symbol('unick')
+// let pro = {
+//     "name" : 'cl',
+//     add : 'clss',
+//     an : 'this',
+//     [sym] : 'symbol' ,
+//     name1 : function(){
+//         console.log(this.add) ;
+//     }
+// } ;
+// console.log(pro['name']) ;
+// console.log(pro.name1()) ;
+
+// let sameAdd = Object.assign({}, profile, pro) ;
+// let sameAdd = {...profile , ...pro} ;
+// let sameAdd = Object.values(profile) ;
+// let sameAdd = Object.keys(pro) ;
+// let sameAdd = Object.entries(profile) ;
+
+// console.log(sameAdd) ;
+
+// let oj = new Object() ;
+// oj.name = 'rahul singh';
+// oj.gmail = 'rahul374774@gmail.com'
+// console.log(oj) ;
+
